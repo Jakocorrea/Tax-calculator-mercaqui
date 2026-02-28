36c338b489a9d49397b424ba73074d1b6cf52ae6
jakoja06 <66153952+jakoja06@users.noreply.github.com>
2026-02-28 15:57:18 -0500

    yip

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/YOUR_REPO_NAME/',
+    base: '/Tax-calculator-mercaqui/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
